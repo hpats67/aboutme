9010906e3d2dd9f48c31ff72eb6b92e8580bd6b3
hpats67 <hpats67@gmail.com>
2016-08-24 20:15:28 -0700

    Second Assignment

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict'
+
+var questionOne = prompt('Have I ever had long hair?');
+  if (questionOne.toLowerCase () === 'yes' || questionOne.toLowerCase () === 'y') {
+    alert("Yeah, you're right I was once a hippy.");
+  }
+  else{
+    alert("No, I'm sorry to say that I once was a hippy. :(");
+  }
+
+var questionTwo = prompt('Would I rather watch movies over listening to music?');
+  if (questionTwo.toUpperCase () === 'YES' || questionTwo.toUpperCase () === 'Y') {
+    alert("Basically, but it does depend on my mood!");
+  }
+  else{
+    alert("Not really, but sometimes I will pick the music instead.");
+  }
+
+var questionThree = prompt("Was I still in Oklahoma in 1980?");
+  if (questionThree.toLowerCase () === 'no' || questionThree.toLowerCase () === 'n') {
+    alert("Trick question!!! I was not born until '83");
+  }
+  else{
+    alert("Trick question!!! I was not born until '83");
+  }
+
+var questionFour = prompt("Do I have a favorite movie?");
+  if (questionFour.toUpperCase () === 'NO' || questionFour.toUpperCase () === 'N') {
+    alert("Yep, I actually have 5 if not more, depends on the genre");
+  }
+  else{
+    alert("No, I have a lot more than that actually!");
+  }
+
+var questionFive = prompt("Would I get a Mac over a PC?");
+  if (questionFive.toLowerCase () === 'yes' || questionFive.toLowerCase () === 'y') {
+    alert("Heck no, ARE YOU CRAZY! I'm a PC");
+  }
+  else{
+    alert("OH YEAH! I am totally a PC");
+  }
